264b6bd4059c1de9d4c5318e93b42e487cbc74b1
lzy-Jerry <763626908@qq.com>
2023-08-21 20:08:09 +0800

    feat: total size

--- plugins/BuildFileSizePlugin.ts
+++ plugins/BuildFileSizePlugin.ts
@@ -2,12 +2,14 @@
  * @Author: lzy-Jerry
  * @Date: 2023-08-20 10:16:58
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-08-20 23:42:59
+ * @LastEditTime: 2023-08-21 20:06:21
  * @Description: 
  */
 // chalk 4.0版本支持cjs
 const chalk = require('chalk');
+const { yellow, green } = chalk
 const toKb = (number) => (number / 1024).toFixed(2)
+
 class BuildFileSizePlugin {
     apply(complier) {
         // 打包输出dist之前
@@ -18,10 +20,10 @@ class BuildFileSizePlugin {
                 const content = compilation.assets[name].source()
                 const size = content.length
                 totalSize += size
-                console.log(chalk.green(`${name} size--->>>`), chalk.yellow(`${toKb(size)}KB`))
+                console.log(green(`${name} size--->>>`), yellow(`${toKb(size)}KB`))
             }
             // console.log('\x1b[33m%s', '我是黄色文字','我还是黄色文字');
-            console.log(`total size--->>>`,chalk.blue(`${toKb(totalSize)}KB`))
+            console.log(yellow(`total size--->>>`,`${toKb(totalSize)}KB`))
         })
         // 打包完成
         complier.hooks.done.tap("buildFileSize", (stas) => {
